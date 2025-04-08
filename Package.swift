6504982c4e0e5607b75c68d8619e037b0a635240
Trevor <trevor.d@beydag.com>
2025-04-08 01:38:05 +0100

    Auto update.

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "DJWUIBuilder",
     platforms: [
-        .iOS(.v10),
+        .iOS(.v13),
         .macOS(.v10_12),
         .tvOS(.v10)
     ],
